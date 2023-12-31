5925cb49c90b51cf1449d7fa1d82dfca29bc7cb3
Alexandre Nicol <alexandre.nicol@qima.com>
2023-12-31 11:27:42 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// =============================================================================
+// VIEWS
+// =============================================================================
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { PhotoComponent } from './views/photo/photo.component';
+import { CameraComponent } from './views/camera/camera.component';
+
+// =============================================================================
+// ROUTES
+// =============================================================================
+const routes: Routes = [
+	{ path: 'login', component: LoginComponent, data: { title: 'Wedding photo' } },
+	{ path: '',   redirectTo: 'login', pathMatch: 'full' },
+	{ path: 'dashboard', component: DashboardComponent, data: { title: 'Wedding photo - dashboard' } },
+	{ path: 'photo', component: PhotoComponent, data: { title: 'Wedding photo - photo' } },
+	{ path: 'camera', component: CameraComponent, data: { title: 'Wedding photo - camera' } },
+]
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// =============================================================================
+// VIEWS
+// =============================================================================
+import { LoginComponent } from './views/login/login.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { PhotoComponent } from './views/photo/photo.component';
+import { CameraComponent } from './views/camera/camera.component';
+
+// =============================================================================
+// COMPONENTS
+// =============================================================================
+
+// =============================================================================
+// DIRECTIVES
+// =============================================================================
+
+// =============================================================================
+// PIPES
+// =============================================================================
+
+// =============================================================================
+// EXTERNAL PACKAGE
+// =============================================================================
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		DashboardComponent,
+		PhotoComponent,
+		CameraComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
